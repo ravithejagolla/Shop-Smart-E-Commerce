478e2bdc1c566bde6e32c016af5793c5a1ea4aa7
Tushar Wasake <tusharwasake@gmail.com>
2025-04-28 21:05:16 +0530

    auth

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -67,6 +67,8 @@ const loginUser = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
+
+    
     res.status(200).json({
       message: "Login successful",
       accessToken,
